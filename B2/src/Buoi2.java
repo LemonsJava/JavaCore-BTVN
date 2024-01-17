2c9d0ad7c4962da13798da91b67a39f41f2fd759
LemonsJava <153980993+LemonsJava@users.noreply.github.com>
2024-01-17 20:34:30 +0700

    BTVN

---
+++ B2/src/Buoi2.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Buoi2 {
+    public static void main(String[] args) {
+
+
+        // Bài 1
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời bạn nhập tên hàng nhập kho: ");
+        String nameProduct = scanner.nextLine();
+
+        System.out.println("Mời bạn nhập ngày tháng năm sinh (yyyy/MM/dd): ");
+        String birthDay = scanner.nextLine();
+        LocalDate localDate = LocalDate.parse(birthDay, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+
+        System.out.println("Mời bạn nhập thời gian nhập hàng (yyyy/MM/dd HH:mm:ss): ");
+        String timeString = scanner.nextLine();
+        LocalDateTime localDateTime = LocalDateTime.parse(timeString, DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+
+        System.out.println("Mời bạn nhập thời gian (HH:mm:ss): ");
+        String timeNow = scanner.nextLine();
+        LocalTime localTime = LocalTime.parse(timeNow, DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+
+
+        System.out.println("----------------");
+        System.out.println("Tên hàng vừa nhập là: " + nameProduct);
+        System.out.println("Ngày tháng năm sinh vừa nhập là: " + localDate);
+        System.out.println("Thời gian nhập hàng là: " + timeString);
+        System.out.println("Thời gian vừa nhập là: " + timeNow);
+
+
+    }
+}
